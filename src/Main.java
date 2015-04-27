a89d0d58e867092506bdaef38a0a2b1f7c53d497
Trond Tufte <trondtufte89@gmail.com>
2015-04-27 17:09:41 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import weka.core.Instances;
+
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("aasdasds");
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		
+		BufferedReader reader = new BufferedReader(
+                new FileReader("/some/where/data.arff"));
+Instances data = new Instances(reader);
+reader.close();
+// setting class attribute
+data.setClassIndex(data.numAttributes() - 1);
+		
+		
+		
 	}
 
 }
