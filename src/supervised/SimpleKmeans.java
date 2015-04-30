166f975053ff14f08958913a7eb1881ad6f30a27
Trond Tufte <trondtufte89@gmail.com>
2015-04-30 17:01:42 +0200

    u

--- src/supervised/SimpleKmeans.java
+++ src/supervised/SimpleKmeans.java
@@ -2,6 +2,7 @@ package supervised;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.PrintWriter;
 
 import weka.clusterers.SimpleKMeans;
 import weka.core.Instances;
@@ -16,7 +17,6 @@ public class SimpleKmeans {
 
 		TextDirectoryToArff tdta = new TextDirectoryToArff();
 		Instances dataset = tdta.createDataset("docs/2");
-
 		// TextDirectoryLoader loader = new TextDirectoryLoader();
 		// loader.setDirectory(new File(""));
 		// Instances dataset = loader.getDataSet();
@@ -29,14 +29,51 @@ public class SimpleKmeans {
 		DataSource source = new DataSource("output.arff");
 		Instances data = source.getDataSet();
 		
+		
+		
 		StringToWordVector filter = new StringToWordVector();
 		filter.setInputFormat(data);
 		Instances dataFiltered = Filter.useFilter(data, filter);
 		
 		SimpleKMeans kmeans = new SimpleKMeans();
-		kmeans.setNumClusters(3);
+		kmeans.setNumClusters(2);
 		kmeans.buildClusterer(dataFiltered);
-		System.out.println(kmeans);
+		
+		//System.out.println(kmeans.clusterInstance(dataFiltered.instance(0)));
+		//System.out.println(data.instance(3));
+		
+		
+		for(int i=0;i<dataFiltered.numInstances();i++){
+			int cluster = kmeans.clusterInstance(dataFiltered.instance(i));
+		//	System.out.println(cluster + " ***** " +dataFiltered.instance(i));
+		//	System.out.println(cluster + " ***** " +data.instance(i));
+			
+		//	System.out.println(data.instance(i));
+			String test = data.instance(i).toString();
+			
+			String work = test.substring(0, test.indexOf(","));
+			System.out.println(work);
+			PrintWriter pr;
+			if(cluster == 0){
+			//	System.out.println("CLUSTER:  " + cluster+"  " + data.instance(i));
+				pr = new PrintWriter(new File("Classified/Heart Failure"));
+				pr.write(work);
+				
+			}else if(cluster == 1){
+			//	System.out.println("CLUSTER1:  " + cluster+"  " + data.instance(i));
+				//data.instance(0).attribute(0);
+				pr = new PrintWriter(new File("Classified/Heart Transplant"));
+				pr.write(work);
+			}
+//			else if(cluster == 2){
+//				System.out.println("CLUSTER2:  " + cluster+"  " + data.instance(i));
+//			}
+			
+		
+		}
+		
+		//System.out.println(kmeans);
+		
 
 	}
 
